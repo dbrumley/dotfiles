42b5687722b4095e3bebe54ea712e213840908b3
David Brumley <dbrumley@cmu.edu>
2014-10-21 13:51:37 -0400

    added ido mode

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,6 @@
 (package-initialize)
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -9,7 +10,12 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(inhibit-default-init nil)
+ '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
+ '(nyan-mode nil)
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
