e308b70824ea8816f7ec749030670a5c09c03173
David Brumley <dbrumley@cmu.edu>
2016-04-21 09:56:15 -0400

    update to configurations

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,3 @@
+(package-initialize)
 (require 'package)
-(add-to-list 'load-path "~/.emacs.d/elisp/org-mode/lisp")
-(add-to-list 'load-path "~/.emacs.d/elisp/org-mode/contrib/lisp" t)
 (org-babel-load-file "~/.emacs.d/dbrumley.org")
